1d14ba698795e3837f1edf9ce1d8bea87f386476
Xu Haiming <haiming12322@qq.com>
2015-07-13 23:50:46 +0800

    Fix dependency problem on github pages

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-define(['jquery', 'scrollzer', 'scrolly', 'util', 'skel' ],
+define(['scrollzer', 'scrolly', 'util', 'skel' ],
 	function($) {
 
 	skel.breakpoints({
--- assets/js/lib/util.js
+++ assets/js/lib/util.js
@@ -1,4 +1,4 @@
-define(['jquery', 'scrollzer', 'scrolly'],
+define(['scrollzer', 'scrolly'],
 	function($) {
 
 	/**
