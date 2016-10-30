c8820918b203c727e353df540d6b36fdf68cd451
Xu Haiming <haiming12322@qq.com>
2016-10-30 14:41:10 +0800

    Build vue 2 start project with vue loader

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # xuhaiming.github.io
 
-My personal tech blog
+> Haiming's tech blog written in Vue JS 2.0
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  $color: blue;
+
+  body {
+    background-color: $color;
+  }
+</style>
