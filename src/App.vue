a31576bfdf1a369bba8a886ea6948aa70d55659f
Xu Haiming <haiming12322@qq.com>
2016-11-06 23:51:27 +0800

    Update banner image and part of content

--- src/App.vue
+++ src/App.vue
@@ -4,61 +4,52 @@
       <div class="nav-wrapper container">
         <a id="logo-container" href="#" class="brand-logo">Haiming Pages</a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="#">Navbar Link</a></li>
+          <li><a class="waves-effect waves-light" href="#">Posts</a></li>
         </ul>
 
         <ul id="nav-mobile" class="side-nav">
-          <li><a href="#">Navbar Link</a></li>
+          <li><a href="#">Navigation</a></li>
         </ul>
-        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Icon</i></a>
       </div>
     </nav>
 
-    <div id="index-banner" class="parallax-container">
+    <div id="index-banner" class="parallax-container valign-wrapper">
       <div class="section no-pad-bot">
         <div class="container">
-          <br><br>
-          <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
           <div class="row center">
-            <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            <h3 class="header center grey-text text-lighten-2">Welcome to Haiming Pages</h3>
+            <br><br>
+            <h5 class="header col s12 light">This is Haiming Xu's personal homepage using Vue JS 2.0 and Materialize CSS hosted on Github Pages.</h5>
           </div>
-          <div class="row center">
-            <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
-          </div>
-          <br><br>
-
         </div>
       </div>
-      <div class="parallax"><img src="./assets/logo.png" alt="Unsplashed background img 1"></div>
+      <div class="parallax"><img src="./assets/banner-1.jpg" alt="Unsplashed background img 1"></div>
     </div>
-
 
     <div class="container">
       <div class="section">
 
         <div class="row">
-          <div class="col s12 m4">
+          <div class="col s12 l4">
             <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-              <h5 class="center">Speeds up development</h5>
+              <h5 class="center">Web Development</h5>
 
-              <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+              <p class="light">Since ECMA Script 2015 and Node JS 7.0 released, Javascript has became to one of the most powerful programming language. Great frameworks such as React JS and Vue JS with Webpack make this world more beautiful.</p>
             </div>
           </div>
 
-          <div class="col s12 m4">
+          <div class="col s12 l4">
             <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-              <h5 class="center">User Experience Focused</h5>
+              <h5 class="center">.NET Platforms</h5>
 
               <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
             </div>
           </div>
 
-          <div class="col s12 m4">
+          <div class="col s12 l4">
             <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-              <h5 class="center">Easy to work with</h5>
+              <h5 class="center">Virtual Reality</h5>
 
               <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
             </div>
@@ -73,11 +64,11 @@
       <div class="section no-pad-bot">
         <div class="container">
           <div class="row center">
-            <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            <h5 class="header col s12 light">Develop native or web application across various of platforms</h5>
           </div>
         </div>
       </div>
-      <div class="parallax"><img src="./assets/logo.png" alt="Unsplashed background img 2"></div>
+      <div class="parallax"><img src="./assets/banner-2.jpg" alt="Unsplashed background img 2"></div>
     </div>
 
     <div class="container">
@@ -86,7 +77,7 @@
         <div class="row">
           <div class="col s12 center">
             <h3><i class="mdi-content-send brown-text"></i></h3>
-            <h4>Contact Us</h4>
+            <h4>About Me</h4>
             <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
           </div>
         </div>
@@ -103,34 +94,18 @@
           </div>
         </div>
       </div>
-      <div class="parallax"><img src="./assets/logo.png" alt="Unsplashed background img 3"></div>
+      <div class="parallax"><img src="./assets/banner-1.jpg" alt="Unsplashed background img 3"></div>
     </div>
 
-    <footer class="page-footer teal">
+    <footer class="page-footer grey darken-4">
       <div class="container">
         <div class="row">
-          <div class="col l6 s12">
-            <h5 class="white-text">Company Bio</h5>
-            <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
-          </div>
           <div class="col l3 s12">
-            <h5 class="white-text">Settings</h5>
+            <h5 class="white-text">Contacts</h5>
             <ul>
-              <li><a class="white-text" href="#!">Link 1</a></li>
-              <li><a class="white-text" href="#!">Link 2</a></li>
-              <li><a class="white-text" href="#!">Link 3</a></li>
-              <li><a class="white-text" href="#!">Link 4</a></li>
-            </ul>
-          </div>
-          <div class="col l3 s12">
-            <h5 class="white-text">Connect</h5>
-            <ul>
-              <li><a class="white-text" href="#!">Link 1</a></li>
-              <li><a class="white-text" href="#!">Link 2</a></li>
-              <li><a class="white-text" href="#!">Link 3</a></li>
-              <li><a class="white-text" href="#!">Link 4</a></li>
+              <li><a class="white-text" href="mailto:haiming12322@qq.com">Email</a></li>
+              <li><a class="white-text" href="https://github.com/xuhaiming">Github</a></li>
+              <li><a class="white-text" href="https://www.linkedin.com/in/xu-haiming-48502869">LinkedIn</a></li>
             </ul>
           </div>
         </div>
@@ -175,36 +150,15 @@ p {
 }
 
 .parallax-container {
-  min-height: 380px;
+  min-height: 550px;
   line-height: 0;
   height: auto;
   color: rgba(255,255,255,.9);
-}
-  .parallax-container .section {
+  width: 100%;
+
+  & .section {
     width: 100%;
   }
-
-@media only screen and (max-width : 992px) {
-  .parallax-container .section {
-    position: absolute;
-    top: 40%;
-  }
-  #index-banner .section {
-    top: 10%;
-  }
-}
-
-@media only screen and (max-width : 600px) {
-  #index-banner .section {
-    top: 0;
-  }
-}
-
-.icon-block {
-  padding: 0 15px;
-}
-.icon-block .material-icons {
-  font-size: inherit;
 }
 
 footer.page-footer {
