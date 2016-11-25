7d6546e52e028e0591a0ae251d2e92aad08ac7cb
Haiming Xu <Haiming_Xu@epam.com>
2016-11-25 17:07:57 +0800

    Add parallax text effect for banner

--- src/home/banner.vue
+++ src/home/banner.vue
@@ -1,18 +1,69 @@
 <template>
-    <div class="parallax-container">
-        <div class="section no-pad-bot vertical-center">
-            <div class="container">
-                <div class="row center">
-                    <h3 class="header center grey-text text-lighten-2">Welcome to Haiming Pages</h3>
-                    <h5 class="header col s12 light">This is Haiming Xu's personal homepage using Vue JS 2.0 and Materialize CSS hosted on Github Pages.</h5>
-                    <a class="waves-effect waves-light btn-large teal darken-1 view-github" target="_blank" href="https://github.com/xuhaiming/xuhaiming.github.io">View on Github</a>
-                </div>
-            </div>
+  <div class="parallax-container banner">
+    <div class="section no-pad-bot vertical-center">
+      <div class="container">
+        <div class="row center" id="scene">
+          <div data-depth="0.60" class="layer">
+            <h3 class="title header center grey-text text-lighten-2">Welcome to Haiming Pages</h3>
+          </div>
+          <div data-depth="0.45" class="layer">
+            <h5 class="description header center col s12 light">This is Haiming Xu's personal homepage using Vue JS 2.0 and Materialize CSS hosted on Github Pages.</h5>
+          </div>
+          <div data-depth="0.50" class="layer view-github_container">
+            <h5 class="view-github header center col s12 light" @click="openGithub">View on Github</h5>  
+          </div>
         </div>
-        <div class="parallax"><img src="../assets/banner-1.jpg" alt="Unsplashed background img 1"></div>
+      </div>
     </div>
+    <div class="parallax"><img src="../assets/banner-1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
 </template>
 
 <script>
-    export default {}
+import Parallax from '../libs/parallax'
+
+export default {
+  mounted() {
+    const scene = document.getElementById('scene')
+    new Parallax(scene, {
+      calibrateX: true,
+      calibrateY: true,
+      invertX: false,
+      invertY: false,
+      limitX: false,
+      limitY: false,
+      scalarX: 10,
+      scalarY: 130,
+      frictionX: 0.2,
+      frictionY: 0.2,
+      originX: 0.5,
+      originY: 0.5
+    });
+  },
+  methods: {
+    openGithub() {
+      window.open('https://github.com/xuhaiming/xuhaiming.github.io', '_blank')
+    }
+  }
+}
 </script>
+
+<style>
+  .banner {
+    & .title {
+      position: relative;
+      top: -50px;
+    }
+
+    & .view-github {
+      position: relative;
+      top: 50px;
+      font-weight: bold;
+      cursor: pointer;
+
+      &_container {
+        width: 100%;
+      }
+    }
+  }
+</style>
