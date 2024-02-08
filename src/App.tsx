c9ba798ff87e67ac8134cdd1fde29bd783291f8e
Haiming Xu <haiming.x@trustana.com>
2024-02-08 12:12:42 +0800

    Update page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -12,17 +9,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Haiming Pages</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Welcome to my empty website
       </p>
     </>
   );
