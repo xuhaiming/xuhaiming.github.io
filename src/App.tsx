5051a6c42ed3a5d1baf6c45c541a140d810c800f
Haiming Xu <haiming.x@trustana.com>
2024-02-08 14:40:00 +0800

    Add tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Haiming Pages</h1>
+      <h1 className="mb-2">Haiming Pages</h1>
       <p className="read-the-docs">
         Welcome to my empty website
       </p>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./src/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
