94bd1b5b4b6774de94c668fb53c5ca1836b99fb5
Xu Haiming <haiming12322@qq.com>
2024-12-04 23:01:46 +0800

    update about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -5,34 +5,44 @@ function AboutMe() {
   return (
     <SectionLayout title="About Me">
       <div className="container mx-auto">
-        <div className="mt-32 flex flex-wrap items-center">
+        <div className="mt-32 flex flex-wrap items-start">
           <div className="mx-auto w-full px-4 md:w-5/12">
             <div className="mb-3 font-bold text-gray-800">
-              Working with me is a pleasure
+              Working experience
             </div>
 
             <div className="mb-8 font-normal text-gray-800">
-              Do not let your uses guess by attaching tooltips and popoves to
-              any element. Just make sure you enable them first via JavaScript.
+              I am currently serving as the Lead Frontend Engineer at Trustana,
+              where I spearhead the development of AI-driven applications.
               <br />
               <br />
-              The kit comes with three pre-built pages to help you get started
-              faster. You can change the text and images and you are good to go.
-              Just make sure you enable them first via JavaScript.
+              Throughout my career, I have collaborated with renowned companies
+              such as Shopee, Tencent, ThoughtWorks, Phillips, and more.
+              <br />
+              <br />
+              My expertise predominantly lies in frontend technologies including
+              React, Next.js, and Node.js. However, I also possess substantial
+              experience with backend and AI-related technologies such as
+              serverless architecture, MongoDB, AI agents leveraging OpenAI
+              APIs, RAG, and model fine-tuning.
+              <br />
+              <br />
+              Additionally, I have contributed to AI-driven image and video
+              generation projects using advanced tools such as Stable Diffusion
+              (SD1.5, SDXL, Flux). I have also worked extensively with ComfyUI
+              workflows, including Lora training, ControlNet, IP Adapter, etc.
             </div>
           </div>
           <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
             <div className="shadow-lg border shadow-gray-500/10 rounded-lg">
-              <div className="relative h-56">
+              <div className="relative">
                 <img
                   alt="Card"
                   src={bg}
                   className="h-full w-full min-w-[400px] min-h-[400px]"
                 />
-              </div>
-              <div>
-                <div color="blue-gray" className="mb-3 mt-2 font-bold">
-                  My LinkedIn QR Code
+                <div className="text-gray-800 text-center mt-2 pt-2 pb-4">
+                  My Linkedin QR Code
                 </div>
               </div>
             </div>
